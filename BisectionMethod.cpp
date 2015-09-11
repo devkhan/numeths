62970bdbe6a6da688223b5b5650b2aef5e40bb19
Devesh Khandelwal <devesh.khandelwal@outlook.com>
2015-09-11 23:31:23 +0800

    Unorganized changes. Rough work.

---
+++ BisectionMethod.cpp
@@ -0,0 +1,5 @@
+//
+// Created by deves on 9/11/2015.
+//
+
+#include "BisectionMethod.h"
---
+++ BisectionMethod.h
@@ -0,0 +1,24 @@
+//
+// Created by deves on 9/11/2015.
+//
+
+#ifndef NUMETHS_BISECTIONMETHOD_H
+#define NUMETHS_BISECTIONMETHOD_H
+
+
+class BisectionMethod {
+
+private:
+    int A, B;
+    int c;
+
+public:
+
+    BisectionMethod()
+    {
+        
+    }
+};
+
+
+#endif //NUMETHS_BISECTIONMETHOD_H
