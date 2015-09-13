bfc836520eb2fe986e0b89db42411b2d317fb0e3
Devesh Khandelwal <devesh.khandelwal@outlook.com>
2015-09-13 18:47:57 +0530

    Base class for root finding method added.

---
+++ RootFindingMethod.cpp
@@ -0,0 +1,5 @@
+//
+// Created by deves on 9/13/2015.
+//
+
+#include "RootFindingMethod.h"
---
+++ RootFindingMethod.h
@@ -0,0 +1,20 @@
+//
+// Created by deves on 9/13/2015.
+//
+
+#ifndef NUMETHS_ROOTFINDINGMETHOD_H
+#define NUMETHS_ROOTFINDINGMETHOD_H
+
+
+class RootFindingMethod
+{
+	protected:
+
+		int IterationCount;
+		double Tolerance;
+
+		enum
+};
+
+
+#endif //NUMETHS_ROOTFINDINGMETHOD_H
