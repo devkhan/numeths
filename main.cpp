3e4f59ae6a3a8413cab54d5947498af4106227cf
Devesh Khandelwal <devesh.khandelwal@outlook.com>
2015-09-18 11:25:23 +0530

    Setup changes: few hacks, seperate demo and library.

--- main.cpp
+++ main.cpp
@@ -4,10 +4,19 @@
 using namespace std;
 using namespace numeths;
 
+// Hack - Used in place of getch();
+char stoppage;
+
+double function(double x)
+{
+	return 3*x*x*x + 45*x*x - 23*x + 3;
+}
+
 int main()
 {
     RootFindingMethod rootFindingMethod;
     rootFindingMethod.setIterationCount(10);
     cout << "Iteration Count: " << rootFindingMethod.getIterationCount() << endl;
+	cin >> stoppage;
     return 0;
 }
