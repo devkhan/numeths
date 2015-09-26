c0b63bb7a57548f65e798b6b3b597b625d7c0a7a
Devesh Khandelwal <devesh.khandelwal@outlook.com>
2015-09-26 23:01:30 +0530

    Matrix input done.

---
+++ gauss_elimination.cpp
@@ -0,0 +1,27 @@
+#include <boost/numeric/ublas/io.hpp>
+#include <boost/numeric/ublas/matrix.hpp>
+
+using namespace std;
+namespace ublas = boost::numeric::ublas;
+
+ublas::matrix<double> _matrix;
+
+int main ()
+{    
+	int size;
+	cout << "Enter number of unknowns: ";
+	cin >> size;
+
+	_matrix.resize(size, size, false);
+
+	for (unsigned i = 0; i < _matrix.size1 (); ++ i)
+		for (unsigned j = 0; j < _matrix.size2 (); ++ j)
+		{
+			cout << "Enter a" << i+1 << j+1 << " : ";
+			cin >> _matrix (i, j);
+		}
+	
+	cout << "matrix: " << endl;
+	cout << _matrix ;
+	
+}
