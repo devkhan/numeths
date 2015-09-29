6e4967334ba71880027a764c5670efc4deaaab8f
Devesh Khandelwal <devesh.khandelwal@outlook.com>
2015-09-29 23:26:49 +0530

    Regula Falsi class init.

---
+++ RegulaFalsiMethod.cpp
@@ -0,0 +1,5 @@
+//
+// Created by deves on 9/29/2015.
+//
+
+#include "RegulaFalsiMethod.h"
---
+++ RegulaFalsiMethod.h
@@ -0,0 +1,17 @@
+//
+// Created by deves on 9/29/2015.
+//
+
+#include "RootFindingMethod.h"
+
+#ifndef NUMETHS_REGULAFALSIMETHOD_H
+#define NUMETHS_REGULAFALSIMETHOD_H
+
+
+class RegulaFalsiMethod : public 
+{
+
+};
+
+
+#endif //NUMETHS_REGULAFALSIMETHOD_H
