94b9eefd317bcedc1ebc2cf770817cd02e3a5583
Devesh Khandelwal <devesh.khandelwal@outlook.com>
2015-10-21 08:06:36 +0530

    Secant method init.

---
+++ SecantMethod.cpp
@@ -0,0 +1,10 @@
+//
+// Created by deves on 10/21/2015.
+//
+
+#include "SecantMethod.h"
+
+namespace numeths
+{
+    
+}
---
+++ SecantMethod.h
@@ -0,0 +1,18 @@
+//
+// Created by deves on 10/21/2015.
+//
+
+#ifndef NUMETHS_SECANTMETHOD_H
+#define NUMETHS_SECANTMETHOD_H
+
+#include "RootFindingMethod.h"
+
+namespace numeths
+{
+    class SecantMethod : RootFindingMethod
+    {
+
+    };
+}
+
+#endif //NUMETHS_SECANTMETHOD_H
