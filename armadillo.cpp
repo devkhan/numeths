77b6c00bffd8447870b0d2c2a21cb1266c3f4667
Devesh Khandelwal <devesh.khandelwal@outlook.com>
2015-10-24 23:54:41 +0530

    Trying to setup armadillo.

--- armadillo.cpp
+++ armadillo.cpp
@@ -4,8 +4,10 @@ using namespace std;
 using namespace arma;
 int main(int argc, char** argv)
 {
-mat A = randu<mat>(4,5);
-mat B = randu<mat>(4,5);
-cout << A * B.t() << endl;
+mat A = randu<mat>(5,5);
+mat B = randu<mat>(5,5);
+cout << B.t() << endl;
+// cout << A*B << endl;
+cout<<"Check."<<endl;
 return 0;
 }
