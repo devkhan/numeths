adf65fcee44a4f3286558afc1a4da4f102c86910
Devesh Khandelwal <devesh.khandelwal@outlook.com>
2015-10-30 23:23:12 +0530

    Documenting Gaussian Elimination.

--- gauss_elimination.cpp
+++ gauss_elimination.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
 #include <armadillo>
+#include <stdlib.h>
+
+#define SINGULARITY_MARGIN 0.00001
 
 using namespace std;
 using namespace arma;
 
+/**
+ * main function. Inputs a system of equations and outputs the solution to that system, if exists.
+ * 
+ * @param  argc Number of command line arguments.
+ * @param  argv Command line arguments.
+ * @return      Exit Status.
+ */
 int main(int argc, char** argv)
 {
 	int vars, equations;
 
+	// Default system if nothing is provided.
 	mat aug_mat = {
 		{1, 2, 3, 1},
 		{4, 5, 6, 1},
@@ -22,6 +33,7 @@ int main(int argc, char** argv)
 
 	aug_mat.resize(equations, vars+1);
 
+	// Input equation coefficients. Complete augmented matrix.
 	for (unsigned i = 0; i < aug_mat.n_rows ; ++i)
 	{
 		cout << "Enter equation " << i+1 << " : ";
@@ -31,8 +43,16 @@ int main(int argc, char** argv)
 		}
 	}
 
+	if (sqrt(square(det(aug_mat.submat(0, 0, aug_mat.n_rows-1, aug_mat.n_cols-2)))))
+	{
+		cout << "Error: Coeffecient matrix is singular. No solution exists.";
+		return EXIT_FAILURE;
+	}
+
 	aug_mat.print("\nAugmented matrix before elimination:\n");
 
+	// Eliminate elemnts below the main diagonal i.e. make them zero using elementary
+	// row operations.
 	for (int i = 0; i < aug_mat.n_rows-1; ++i)
 	{
 		
@@ -45,6 +65,8 @@ int main(int argc, char** argv)
 
 	aug_mat.print("\nAugmented matrix after lower elimination:\n");
 
+	// Eliminate elemnts above the main diagonal i.e. make them zero using elementary
+	// row operations.
 	for (int i = aug_mat.n_rows-1; i > 0; --i)
 	{
 		for (int j = i-1; j >= 0; --j)
@@ -56,9 +78,11 @@ int main(int argc, char** argv)
 
 	aug_mat.print("\nAugmented matrix after upper elimination:\n");
 
+	// Substituting a(i,i)*x(i) = b(i). Calculating x(i).
+	// Done by dividing the b vector by the diagonal of the coeffecient matrix.
 	vec b = aug_mat.col(aug_mat.n_cols-1)/aug_mat.diag();
 
 	b.print("\nSolutions:\n");
 
-	return 0;
+	return EXIT_SUCCESS;
 }
